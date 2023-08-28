644e660ff1d6344de888f4ac3bce200952fed343
Boris Belyaev <bbor1974@gmail.com>
2023-08-28 22:39:06 +0400

    ipv6: update ip6_rt_last_gc every time GC is run
    commit 49a18d86f66d33a20144ecb5a34bba0d1856b260 upstream.
    
    As pointed out by Eric Dumazet, net->ipv6.ip6_rt_last_gc should
    hold the last time garbage collector was run so that we should
    update it whenever fib6_run_gc() calls fib6_clean_all(), not only
    if we got there from ip6_dst_gc().
    
    Change-Id: I2aa9fa435b251f712bba37061f576f1f00e16e0f
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -1576,6 +1576,8 @@ static DEFINE_SPINLOCK(fib6_gc_lock);
 
 void fib6_run_gc(unsigned long expires, struct net *net)
 {
+	unsigned long now;
+
 	if (expires != ~0UL) {
 		spin_lock_bh(&fib6_gc_lock);
 		gc_args.timeout = expires ? (int)expires :
@@ -1591,10 +1593,12 @@ void fib6_run_gc(unsigned long expires, struct net *net)
 	gc_args.more = icmp6_dst_gc();
 
 	fib6_clean_all(net, fib6_age, 0, NULL);
+	now = jiffies;
+	net->ipv6.ip6_rt_last_gc = now;
 
 	if (gc_args.more)
 		mod_timer(&net->ipv6.ip6_fib_timer,
-			  round_jiffies(jiffies
+			  round_jiffies(now
 					+ net->ipv6.sysctl.ip6_rt_gc_interval));
 	else
 		del_timer(&net->ipv6.ip6_fib_timer);
