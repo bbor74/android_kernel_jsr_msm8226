751536fcb8b85baac510985064cf745e331c7b8c
Boris Belyaev <bbor1974@gmail.com>
2023-09-08 21:03:45 +0400

    Revert "ipv6: update ip6_rt_last_gc every time GC is run"
    This reverts commit 644e660ff1d6344de888f4ac3bce200952fed343.

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -1595,8 +1595,6 @@ static DEFINE_SPINLOCK(fib6_gc_lock);
 
 void fib6_run_gc(unsigned long expires, struct net *net)
 {
-	unsigned long now;
-
 	if (expires != ~0UL) {
 		spin_lock_bh(&fib6_gc_lock);
 		gc_args.timeout = expires ? (int)expires :
@@ -1612,12 +1610,10 @@ void fib6_run_gc(unsigned long expires, struct net *net)
 	gc_args.more = icmp6_dst_gc();
 
 	fib6_clean_all(net, fib6_age, 0, NULL);
-	now = jiffies;
-	net->ipv6.ip6_rt_last_gc = now;
 
 	if (gc_args.more)
 		mod_timer(&net->ipv6.ip6_fib_timer,
-			  round_jiffies(now
+			  round_jiffies(jiffies
 					+ net->ipv6.sysctl.ip6_rt_gc_interval));
 	else
 		del_timer(&net->ipv6.ip6_fib_timer);
