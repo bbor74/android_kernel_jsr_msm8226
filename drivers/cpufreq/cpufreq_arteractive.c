4d874c9b30870f1c221eba525631bfe4cb1ae9c1
Boris Belyaev <bbor1974@gmail.com>
2023-09-08 19:17:00 +0400

    pufreq: interactive: Fix build warning
    ../../../../../../kernel/jsr/msm8226/drivers/cpufreq/cpufreq_arteractive.c: In function 'update_load':
    ../../../../../../kernel/jsr/msm8226/drivers/cpufreq/cpufreq_arteractive.c:476:64: warning: left-hand operand of comma expression has no effect [-Wunused-value]
      delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp, io_is_busy);
                                                                    ^
    
    Change-Id: I3970cc3f54e20832e9246ffca433d88fc0e13a1d

--- drivers/cpufreq/cpufreq_arteractive.c
+++ drivers/cpufreq/cpufreq_arteractive.c
@@ -473,7 +473,7 @@ static u64 update_load(int cpu)
 
 	now_idle = get_cpu_idle_time(cpu, &now, io_is_busy);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
-	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp, io_is_busy);
+	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
 	if (delta_time <= delta_idle)
 		active_time = 0;
